2427ab3336ce8def0b66220c8c13208dccf6894e
Niewidzialny84 <niewidzialny84@vp.pl>
2021-06-04 20:24:14 +0200

    categories handle

--- Server/src/user.py
+++ Server/src/user.py
@@ -47,7 +47,7 @@ class User(object):
                 h,p = Protocol.encode(Header.ERR, msg = 'Invalid login data')
                 Logger.log('User login invalid data '+ str(self.address))           
             elif headerType == Header.REG:
-                r = requests.post(URL.local+'users', data=json.dumps({'username':data['login'], 'password':self.passwordHash(data['password']).hex()}))
+                r = requests.post(URL.local+'users', json={'username':data['login'], 'password':self.passwordHash(data['password']).hex()})
                 if r.status_code == 201:
                     h,p = Protocol.encode(Header.ACK, msg = 'Created Account')
                     Logger.log('User registered ')
